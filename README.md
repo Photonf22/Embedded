ddd1d31ce2f8f0381bbb62c6bb1a6d2ed0303293
Alejandro Sanchez <Sanchez.alef22@gmail.com>
2022-05-09 13:59:15 -0600

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,4 @@
+/* Author: Alejandro Sanchez
+Project Description: In this small project we will be sorting a set array from
+large to small and then run some caculations on this data to find things such as the median, mean, maximum, minimum, etc
+Repository Description: The point of this repository is to show that the student understands how to use vim, git, and create simple files and compile them.
---
+++ stats.h
@@ -0,0 +1,66 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief In this header file all the functions are defined that will be used in stats.c and in this project
+ *
+ *
+ * @author Alejandro Sanchez
+ * @date 05-02-2022
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* This function will print all the statistics of the array given*/
+/* @param unsigned char *ptr which is a pointer to the first place in memory of the array*/
+/* @return calculated statistics of given array*/
+double print_statistics(void);
+/* This function will simply print the array */
+/* @param unsigned char *ptr which is a pointer to the first place in memory of the array and length of array*/
+/* @return all values inside array*/
+int print_array(unsigned char *ptr, unsigned length);
+/* This function will find the median of the array*/
+/* @param unsigned char *ptr which is a pointer to the first place in memory of the array and length of array*/
+/* @return median of array*/
+double find_median(unsigned char *ptr, unsigned length);
+/* This function will find the mean of the given array*/
+/* @param unsigned char *ptr which is a pointer to the first place in memory of the array and length of array*/
+/* @return mean of given array*/
+double find_mean(unsigned char *ptr);
+/* This function will find the maximum of the given array*/
+/* @param unsigned char *ptr which is a pointer to the first place in memory of the array and length of array*/
+/* @return max of given array*/
+double find_maximum(unsigned char *ptr);
+/* This function will find the minimum of the given array*/
+/* @param unsigned char *ptr which is a pointer to the first place in memory of the array and length of array*/
+/* @return min of given array*/
+double find_minimum(unsigned char *ptr);
+/* This function will sort the given array */
+/* @param unsigned char *ptr which is a pointer to the first place in memory of the array and length of array*/
+/* return sorted Array*/ 
+void sort_array(unsigned char *ptr);
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
